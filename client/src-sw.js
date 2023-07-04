b2fe7791c26ec46aab8b7d1ebaaa75db62e12d89
pvlln <pv823@nyu.edu>
2023-07-04 18:34:03 -0400

    fixed cache bugs

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -43,4 +43,4 @@ registerRoute(
 
 // Check assetCache, why is offlineFallback not in use?
 
-registerRoute();
+// registerRoute();
